897bf7cef5c984c99512bd808da4b1d0d0a7089b
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-11-16 17:58:51 +0900

    Add files via upload

---
+++ 3_kujibiki/solution.py
@@ -0,0 +1,16 @@
+import unittest
+
+def 
+
+def solution(sum_desired, array_kuji):
+    pass
+
+
+
+
+class TestSolution(unittest.TestCase):
+    def test_1_sample(self):
+        self.assertEqual(solution(3, 10, [1, 3, 5]), True)
+    
+
+unittest.main(argv=['first-arg-is-ignored'], exit=False)
