55dcc5d61be9533fd8a168a26e8b360325c2b2e8
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-11-16 10:13:50 +0900

    Add files via upload

--- 2_ants.py
+++ 2_ants.py
@@ -1,6 +1,6 @@
 import unittest
 
-# 三角形の条件を満たす最大の周の長さを返す
+# ありの落ちるまでの時間を計測する
 def solution(L, X):
     n = len(X)
     Lmin = 0
@@ -15,10 +15,19 @@ def solution(L, X):
     
     return Lmin, Lmax
 
+
 class TestSolution(unittest.TestCase):
     def test_1_sample(self):
         self.assertEqual(solution(10, [2, 6, 7]), (4, 8))
     
+    def test_2_sample(self):
+        self.assertEqual(solution(3, [1]), (1, 2))
+
+    def test_3_sample(self):
+        self.assertEqual(solution(20, [4,5,6]), (6, 16))
+    
+    def test_4_sample(self):
+        self.assertEqual(solution(100, [20,40,60,80,85]), (40, 85))
 
 unittest.main(argv=['first-arg-is-ignored'], exit=False)
 
