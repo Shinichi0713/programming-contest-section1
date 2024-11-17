6d6fd2ddeeba6f82f67101cdb64879dd9d8d8921
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-11-17 18:11:47 +0900

    update

--- 6_lake_count/solution.py
+++ 6_lake_count/solution.py
@@ -5,22 +5,21 @@ import os, copy
 def solution(array_target):
     count_lake = 0
     array_count = copy.deepcopy(array_target)
-
+    for i in range(len(array_count)):
+        for j in range(len(array_count[0])):
+            if array_count[i][j] == "1":
+                count_lake += 1
+                find_neighbor(array_count, i, j)
     return count_lake
 
+# 再帰的に1がなくなるまで近隣探索を行う
 def find_neighbor(array_target, row, col):
-    count = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
-            if i == 0 and j == 0:
-                continue
-            if row + i < 0 or row + i >= len(array_target):
-                continue
-            if col + j < 0 or col + j >= len(array_target[0]):
-                continue
-            if array_target[row + i][col + j] == 1:
-                count += 1
-    return count
+            if 0 <= row + i < len(array_target) and 0 <= col + j < len(array_target[0]):
+                if array_target[row + i][col + j] == "1":
+                    array_target[row + i][col + j] = "0"
+                    find_neighbor(array_target, row + i, col + j)
 
 def read_input(file_target):
     array_read = []
@@ -34,10 +33,10 @@ def read_input(file_target):
 class TestSolution(unittest.TestCase):
     def test_1_sample(self):
         array_read = read_input(f"{os.path.dirname(__file__)}/test.txt")
-        self.assertEqual(array_read, 9)
+        self.assertEqual(solution(array_read), 10)
     
 
 if __name__ == "__main__":
-    array_read = read_input(f"{os.path.dirname(__file__)}/test.txt")
-    solution(array_read)
-    # unittest.main(argv=['first-arg-is-ignored'], exit=False)
+    # array_read = read_input(f"{os.path.dirname(__file__)}/test.txt")
+    # print(solution(array_read))
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
